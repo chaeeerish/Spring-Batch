b5a03d92717827ed8569db3e5494d182909a17f1
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-08-12 17:44:32 +0900

    스프링 배치 실습

---
+++ SpringBatch/src/main/java/org/example/springbatch/SpringBatchApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class SpringBatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchApplication.class, args);
+    }
+}
---
+++ SpringBatch/src/main/java/org/example/springbatch/schedule/FirstSchedule.java
@@ -0,0 +1,34 @@
+package org.example.springbatch.schedule;
+
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Configuration
+public class FirstSchedule {
+    private final JobLauncher jobLauncher;
+    private final JobRegistry jobRegistry;
+
+    public FirstSchedule(JobLauncher jobLauncher, JobRegistry jobRegistry) {
+        this.jobLauncher = jobLauncher;
+        this.jobRegistry = jobRegistry;
+    }
+
+    @Scheduled(cron = "10 * * * * *", zone = "Asia/Seoul") // 매일 매시간 매분 10초마다
+    public void runFirstJob() throws Exception {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-hh-mm-ss");
+        String date = dateFormat.format(new Date());
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("date", date)
+                .toJobParameters();
+
+        jobLauncher.run(jobRegistry.getJob("firstJob"), jobParameters);
+    }
+}
